c4a7cba073bccfa7df629925ee2e4a23d70c0958
jhonatan <jhonatan.s.costa97@gmail.com>
2023-07-05 10:51:35 -0300

    feat(app): add auth component style and apply switch button logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AuthComponent} from "./auth/auth.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'login',
+    component: AuthComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
